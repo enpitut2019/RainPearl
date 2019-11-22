9ee4eb9486ff8b3fcced5d4c49128b26878fc472
atm004 <atm004@a-tm.co.jp>
2019-11-22 15:40:12 +0900

    we are winner

--- led_and_vibe/led_and_vibe.ino
+++ led_and_vibe/led_and_vibe.ino
@@ -4,51 +4,47 @@
 #define PIN            6
 #define NUMPIXELS      40
 
+#define DUTY_INPUT     10
+#define VIB_MOTOR_PIN1 9
+#define VIB_MOTOR_PIN2 8
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int delayval = 20; // delay for half a second
+int frequency = 60;
+float duty_ratio = 0.1;
 
-const int vib_motor_pin = 9;
-const int vib_motor_pin2 = 8;
-
-int state=0;
+int state = 0;
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
-  pinMode(2, INPUT);
+  pinMode(DUTY_INPUT, INPUT);
 }
 
 void loop() {
 
-    if (digitalRead(2) == LOW) {
-      if(state==0){
-        state=1;
-        dalayval+=3;
-      }else{
-      }
-      } else {
-        if(state==0){
-        }else{
-          state=0;
-        }
+  if (digitalRead(DUTY_INPUT) == HIGH) {
+    if( state == 0 ){
+//      duty_ratio = (((int)(duty_ratio * 10) + 1) % 10) / 10.0;
+        frequency = (frequency + 5) % 50 + 50;
     }
-
+    state = 1;
+  } else {
+    state = 0;
+  }
   
-  analogWrite( vib_motor_pin, 250 );
-  analogWrite( vib_motor_pin2, 250 );
-    
+  analogWrite( VIB_MOTOR_PIN1, 250 );
+  analogWrite( VIB_MOTOR_PIN2, 250 );
+
+  float delaytime = 1.0 / frequency * 1000;
+  
+  for(int i=0;i <NUMPIXELS; i++){
+    pixels.setPixelColor(i, pixels.Color(150,150,150)); // Moderately bright green color.
+  }
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(delaytime * duty_ratio); // Delay for a period of time (in milliseconds).
+
   for(int i=0;i <NUMPIXELS; i++){
     pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
   }
-
-  for(int i=0;i <NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
-  }
-
-
- // This sends the updated pixel color to the hardware.
-   
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(delaytime * (1.0 - duty_ratio)); // Delay for a period of time (in milliseconds).   
 }
